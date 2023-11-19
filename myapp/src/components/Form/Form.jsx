a2d0f821c1f20fd555e40c94cc6a1c143d01912d
Emanuel Evangelista <emanu.vange@gmail.com>
2023-11-19 09:53:21 -0300

    Creado el TP6

--- myapp/src/components/Form/Form.jsx
+++ myapp/src/components/Form/Form.jsx
@@ -1,70 +1,57 @@
 import React, { useState } from "react";
 import './Form.css'
 export default function Form() {
-    const [form, setForm] = useState({
-        firstName: '',
-        lastName: "",
-        email: "",
-        tel: ""
-      });
+  const [form, setForm] = useState({ name: '', subName: '', email: '', tel: '', password: '' })
 
-       
-  
+
+  const handleSubmit = (event) => {
+    console.log("handleSubmit", form)
+    event.preventDefault()
+    }
+         
+    const handleChange = (event) => {
+      const name = event.target.name
+      const value = event.target.value
+      console.log("hand leChange", name, value)
+      setForm({ ... form, [name]: value })
+      }
+      
+
     return (
-      <form >
-        <label htmlFor="name">Nombre</label>
+      <form onSubmit={handleSubmit}>
+        <label>Nombre</label>
         <input
           id="name"
           name="name"
           type="text"
-          value={form.firstName}
-          onChange={e => {
-            setForm({
-              ...form,
-              firstName: e.target.value
-            });
-          }}
+          value={form.name}
+          onChange={handleChange}
           
         />
-        <label htmlFor="subnombre">Apellido</label>
+        <label>Apellido</label>
         <input
           id="subname"
           name="subname"
           type="text"
           value={form.lastName}
-          onChange={e => {
-            setForm({
-              ...form,
-              lastName: e.target.value
-            });
-          }}
+          onChange={handleChange}
         />
-         <label htmlFor="email">Email</label>
+         <label>Email</label>
         <input
           id="email"
           name="email"
           type="email"
           value={form.email}
-          onChange={e => {
-            setForm({
-              ...form,
-              email: e.target.value
-            });
-          }}
+          onChange={handleChange}
         />
          
-         <label htmlFor="tel">Teléfono</label>
+         <label>Teléfono</label>
         <input
           id="tel"
           name="tel"
           type="number"
           value={form.tel}
-          onChange={e => {
-            setForm({
-              ...form,
-              tel: e.target.value
-            });
-          }}
+          onChange={handleChange}
         />
         <label htmlFor="password">Password</label>
         <input
@@ -80,7 +67,7 @@ export default function Form() {
           type="password"
         />
         
-        <button type="submit" onSubmit = {console.log(form)}>Enviar</button>
+        <button type="submit">Enviar</button>
       </form>
     );
   }
