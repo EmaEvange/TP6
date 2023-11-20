1192d08fea8674cb0b44710842f9cdae2b32625f
Andres-Tonelli <angatonelli@yahoo.com.ar>
2023-11-20 17:48:17 -0300

    commit-01
    Se realizan cambios para validacion de campos. Se agrega limpieza de campos postsubmit

--- myapp/src/components/Form/Form.jsx
+++ myapp/src/components/Form/Form.jsx
@@ -1,25 +1,76 @@
 import React, { useState } from "react";
 import './Form.css'
 export default function Form() {
-  const [form, setForm] = useState({ name: '', subName: '', email: '', tel: '', password: '' })
+  const [form, setForm] = useState({ name: '', subname: '', email: '', tel: '', password: '',password2: ''})
 
 
   const handleSubmit = (event) => {
-    console.log("handleSubmit", form)
-    event.preventDefault()
-    }
+      event.preventDefault()
+
+      if (!form.name) {
+        alert('EL campo nombre no puede quedar vacio')
+        return null
+      }
+      if (!form.subname) {
+        alert('EL campo apellido no puede quedar vacio')
+        return null
+      }
+      if (!validarEmail(form.email)) {
+        alert('Debe ingresar un email valido')
+        return null
+      }
+      if (!form.tel) {
+        alert('EL campo telefono no puede quedar vacio')
+        return null
+      }
+      console.log(`pass:${form.password}  pass2:${form.password2}`);
+      if (!form.password) {
+        alert('EL campo password no puede quedar vacio')
+        return null;
+      }
+      if (!form.password2) {
+        alert('EL campo password confirm no puede quedar vacio')
+        return null;
+      }
+    if (form.password !== form.password2) {
+        alert('Las contaseñas deben coicidir')
+        return null;
+      }
+      console.log("handleSubmit", form)
+      alert('Registro realizado con exito')
+      limpiarCampos()
+  }
          
     const handleChange = (event) => {
       const name = event.target.name
       const value = event.target.value
-      console.log("hand leChange", name, value)
+      console.log("handleChange", name, value)
       setForm({ ... form, [name]: value })
-      }
+    }
       
+    const validarEmail = (email) => {
+        let patron = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        return patron.test(email)
+     }
+
+    function limpiarCampos() {
+        form.name = ''
+        form.subname = ''
+        form.email = ''
+        form.tel = ''
+        form.password = ''
+        form.password2 = '' 
+        document.getElementById("name").value = ''
+        document.getElementById("subname").value= ''
+        document.getElementById("email").value = ''
+        document.getElementById("tel").value = ''
+        document.getElementById("password").value = ''
+        document.getElementById("password2").value = '' 
+    }
 
     return (
       <form onSubmit={handleSubmit}>
-        <label>Nombre</label>
+        <label>nombre</label>
         <input
           id="name"
           name="name"
@@ -33,7 +84,7 @@ export default function Form() {
           id="subname"
           name="subname"
           type="text"
-          value={form.lastName}
+          value={form.subname}
           onChange={handleChange}
         />
          <label>Email</label>
@@ -58,13 +109,16 @@ export default function Form() {
           id="password"
           name="password"
           type="password"
-          
+          value={form.password}
+          onChange={handleChange}
         />
-        <label htmlFor="password2">Password</label>
+        <label htmlFor="password2">Confirmar Contraseña</label>
         <input
           id="password2"
           name="password2"
           type="password"
+          value={form.password2}
+          onChange={handleChange}
         />
         
         <button type="submit">Enviar</button>
